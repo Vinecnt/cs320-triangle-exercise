7327bdc16345a371ff343bb3989dd19a2b068d11
Jonathan Trott <jont1212@gmail.com>
2018-12-08 19:18:45 -0500

    add more tests

--- test/triangle/TriangleTest.java
+++ test/triangle/TriangleTest.java
@@ -12,9 +12,45 @@ import static triangle.Triangle.Type.*;
 public class TriangleTest {
 
     @Test
-    public void test1() {
+    public void test_triangle() {
         Type actual = Triangle.classify(10, 10, 10);
         Type expected = EQUILATERAL;
         assertEquals(actual, expected);
+
+        actual = Triangle.classify(100, 100, 100);
+        expected = EQUILATERAL;
+        assertEquals(actual, expected);
+
+        actual = Triangle.classify(150, 150, 150);
+        expected = EQUILATERAL;
+        assertEquals(actual, expected);
+        
+        actual = Triangle.classify(20, 30, 40);
+        expected = SCALENE;
+        assertEquals(actual, expected);
+        
+        actual = Triangle.classify(40, 30, 20);
+        expected = SCALENE;
+        assertEquals(actual, expected);
+        
+        actual = Triangle.classify(20, 40, 30);
+        expected = SCALENE;
+        assertEquals(actual, expected);
+        
+        actual = Triangle.classify(5, 5, 2);
+        expected = ISOSCELES;
+        assertEquals(actual, expected);
+        
+        actual = Triangle.classify(5, 2, 5);
+        expected = ISOSCELES;
+        assertEquals(actual, expected);
+        
+        actual = Triangle.classify(2, 5, 5);
+        expected = ISOSCELES;
+        assertEquals(actual, expected);
+        
+        
     }
+    
+   
 }
